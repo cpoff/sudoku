cfde7cc05c0547aa93ff85c8224fcf7a72b15edc
cpoff <curt.poff@gmail.com>
2015-06-19 09:05:54 -0700

    Replaced outdated tests.

--- test/test.js
+++ test/test.js
@@ -4,81 +4,76 @@ var assert = require('assert');
 var chai = require('chai');
 var expect = chai.expect;
 
-var fakeArray = [1,2,3,4,5,6];
+var possibleNums = [1,2,3,4,5,6,7,8,9];
 
-describe('My fake array object', function() {
-  describe('The pop method', function() {
-    it('should return the final element', function() {
-      assert.equal(fakeArray.pop(), 6);
-    });
-  });
-});
+describe('remaining', function() {
+    it('should return the number of squares left to solve', function() {
+        expect(game.remaining()).to.be.a('number');
+        expect(game.remaining()).to.be.less.than(81);
+        expect(game.remaining()).to.be.more.than(-1);
+    })
+})
 
-describe('The post-pop array length', function() {
-    it('should return the new array length', function() {
-        expect(fakeArray.length).to.equal(5);
-    });
-});
-
-describe('Populated array', function() {
-        it('array should have contents', function() {
-        expect(fakeArray).to.not.be.empty;
-  });
-});
-
-describe('Contents of the array items', function() {
-        it('should all be integers', function() {
-        expect(fakeArray).to.not.contain.a('string');
+describe('confirm', function() {
+  it('sum of completed row/col/block to be 45', function() {
+      expect(game.confirm.row()).to.equal.sum.of(45);
+      expect(game.confirm.col()).to.equal.sum.of(45);
+      expect(game.confirm.block()).to.equal.sum.of(45);
   })
 });
 
-//describe('This', function() {
-//    it('should return [1,2,3,4,5,6]',function(){
-//        expect(fakeArray.map).to.contain(1,2,3,4,5,6);
+describe('get', function() {
+  it('return value of given square', function() {
+      expect(square.finished()).to.be.a('number');
+      expect(square.notFinished()).to.equal('.');
+    })
+});
+
+describe('Game', function() {
+    describe('possibleNums', function() {
+        it('lists the number(s) that are available to go into a square')
+            expect(squares.possibleNums()).to.be.lessThan(10)
+            expect(squares.possibleNums()).to.be.moreThan(-1)
+            expect(squares.possibleNums()).to.NOT.BE.ANYWHERE.ELSE.IN.THE.ROW()
+            expect(squares.possibleNums()).to.NOT.BE.ANYWHERE.ELSE.IN.THE.COL()
+            expect(squares.possibleNums()).to.NOT.BE.ANYWHERE.ELSE.IN.THE.BLOCK()
+    })
+});
+
+describe('Game', function() {
+    describe('lookSquares', function() {
+      it('return the number of unfinished squares remaining in row/col/block.')
+          expect(square.finished()).to.be.a('number');
+          expect(square.notFinished()).to.equal('.');
+    })
+});
+
+describe('locateRow', function() {
+  it('should return a row number within range 0-8. based on math', function() {
+      expect(game.locateRow(squareNum)).to.be.a('number');
+      expect(game.locateRow(17)).to.equal(1);
+      expect(game.locateRow(0)).to.be.a(0);
+      expect(game.locateRow(80)).to.be.a(8);
+      expect(game.locateRow(82)).to.throw();
+    })
+});
+
+
+//---------------------------
+//describe('The post-pop array length', function() {
+//    it('should return the new array length', function() {
+//        expect(fakeArray.length).to.equal(5);
+//    });
+//});
+//
+//describe('Populated array', function() {
+//        it('array should have contents', function() {
+//        expect(fakeArray).to.not.be.empty;
+//  });
+//});
+//
+//describe('Contents of the array items', function() {
+//        it('should all be integers', function() {
+//        expect(fakeArray).to.not.contain.a('string');
 //  })
 //});
-
-
-//describe('Map deep equals', function() {
-//            it('should return [1,2,3]',function(){
-//            assert.equalfakeArray.map),[1,2,3]
-//            )}
-//    });
-
-//
-//
-//var loadFactors = fakeArray.map(function(){
-//    return elevator.loadFactor()
-//})
-
-//
-//expect({ foo: 1, bar: 2 }).to.have.any.keys('foo', 'baz');
-//expect({ foo: 1, bar: 2 }).to.have.any.keys('foo');
-//expect({ foo: 1, bar: 2 }).to.contain.any.keys('bar', 'baz');
-//expect({ foo: 1, bar: 2 }).to.contain.any.keys(['foo']);
-//expect({ foo: 1, bar: 2 }).to.contain.any.keys({'foo': 6});
-//expect({ foo: 1, bar: 2 }).to.have.all.keys(['bar', 'foo']);
-//expect({ foo: 1, bar: 2 }).to.have.all.keys({'bar': 6, 'foo', 7});
-//expect({ foo: 1, bar: 2, baz: 3 }).to.contain.all.keys(['bar', 'foo']);
-//expect({ foo: 1, bar: 2, baz: 3 }).to.contain.all.keys([{'bar': 6}}]);
-//
-//
-//describe('Array', function(){
-//  describe('#indexOf()', function(){
-//    it('should return -1 when the value is not present', function(){
-//      assert.equal(-1, [1,2,3].indexOf(5));
-//      assert.equal(-1, [1,2,3].indexOf(0));
-//    })
-//  })
-//})
-//
-
-//describe('Array', function(){
-//  describe('#indexOf()', function(){
-//    it('should return -1 when the value is not present', function(){
-//      assert.equal(-1, [1,2,3].indexOf(5));
-//      assert.equal(-1, [1,2,3].indexOf(0));
-//    })
-//  })
-//})
-
